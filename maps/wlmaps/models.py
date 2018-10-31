065c691abe373bcbba3bc24629358746e898e0d5
James Yang <yang1274@purdue.edu>
2018-10-30 20:42:15 -0400

    Add twitter api files

--- maps/wlmaps/models.py
+++ maps/wlmaps/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+#class Mapinfo(models.Model):
+    #"""docstring forMapinfo."""
+    #def __init__(self, arg):
+        #superMapinfo, self).__init__()
+        #self.arg = arg
