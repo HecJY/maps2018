eb467474db23b3f7569505efc19124473b0ff9c0
James Yang <yang1274@purdue.edu>
2018-10-31 15:12:21 -0400

    Try to fix the exceptions

--- twtapi/twtapi.py
+++ twtapi/twtapi.py
@@ -1,4 +1,5 @@
 import json
+from twython import TwythonStreamer
 from twython import Twython
 import csv
 
@@ -11,9 +12,9 @@ python_tweets = Twython(creds['CONSUMER_KEY'],creds['CONSUMER_SECRET'])
 
 
 #create a q
-query = {'q':'',
+query = {'q':' ',
         'result_type':'recent',
-        'geocode':'-86.887 40.428 10mi',
+        #'geocode':'-86.887 40.428 10mi',
         'lang':'en',
         }
 
@@ -26,22 +27,34 @@ def tw_info(tweet):
     return d
 
 
-class twStreamer(Twython):
+class twStreamer(TwythonStreamer):
     def on_sucess(self,data):
+        print(data)
         if data['lang'] == 'en':
             tweet_inf = tw_info(data)
             self.save_to_csv(tweet_inf)
 
     def on_error(self, status_code, data):
+        print("111111")
         print(status_code,data)
         self.disconnect()
 
     def save_to_csv(self, tweet):
+        print(list(tweet.values()))
         with open(r'saved_tweets.csv','a') as file:
             writer = csv.writer(file)
             writer.writerow(list(tweet.values()))
-
-stream = twStreamer(creds)
+#stream = twStreamer(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'], creds['ACCESS_TOKEN'], creds['ACCESS_SECRET'])
+stream = twStreamer('x4FguSwehXpgdPtq2bTi5dOJU', 'yAHfjLyxuct4I6He9uDbqr547MN0RP6ghdSFgYn19XDF4uXbge',
+'2219948304-c60sVwWulIqRJLnPSFH6rMWn9uMIvYxmQSTQx56', 'dH11u0vsbWIiMBzgEbHTgpFSzlrmu0vhyGfqbXiRsBsUf')
 
 #Find out what are things that are neccessary for streaming
-stream.statuses.filter(query)
+
+try:
+    print("start")
+    stream.statuses.filter(locations = "-86.96,40.41,-86.87,40.49")
+    print("end")
+except:
+    print("Oops!",sys.exc_info()[0],"occured.")
+    print("Next entry.")
+    print()
